fc7da7d27cbe6bb939513d435ffd449835518574
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-20 14:32:50 +0800

    pembagian class

--- Crushgear.cpp
+++ Crushgear.cpp
@@ -2,5 +2,17 @@
 #include <string>
 using namespace std;
 
+class CrushGear{
+	public:
+		void bemotoran(string main);
+		void hoby(string hoby);
+			 Mainan(string Mainan);
+			 Barang_mahal(string Barang);
+			 Langka(string Langka); 
+		string frontweapon, rearweapon , typecasis, specialattack;
+
+};
+
+
 int main() {
 }
